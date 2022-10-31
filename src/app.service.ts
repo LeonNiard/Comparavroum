c51196f9123a0ea0a0cbb15a3c4226fc9155ab45
Mohamed Midjek <mohamed.midjek@enigma-school.com>
2022-10-31 14:52:17 +0100

    feat : ajout des appels liste des voitures/details voiture

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { getCarList} from "./api/api.client";
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getCarList(): void {
+    return getCarList();
+  }
+
+  getDetailsCar(carId): void {
+    return this.getDetailsCar(carId);
   }
 }
